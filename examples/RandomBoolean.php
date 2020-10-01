931208d8d1a0b1074035f19d6c4a1b627d1654bd
Chris Bongers <chrisbongers@gmail.com>
2020-10-01 16:52:00 +0200

    Added random boolean example script

---
+++ examples/RandomBoolean.php
@@ -0,0 +1,14 @@
+<?php
+require_once "./vendor/autoload.php";
+
+use HiFolks\RandoPhp\Randomize;
+
+echo "--- Is he telling the truth? ".PHP_EOL;
+$randomBool = Randomize::boolean()->generate();
+echo ($randomBool) ? "Yes": "No" ;
+echo PHP_EOL. "-----------------".PHP_EOL;
+
+echo "--- Show me a proper boolean: ".PHP_EOL;
+$randomBool = Randomize::boolean()->generate();
+echo $randomBool;
+echo PHP_EOL. "-----------------".PHP_EOL;
