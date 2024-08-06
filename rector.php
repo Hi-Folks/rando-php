b81f7875419c3192b89089bdb6140ce2aa2d81d4
Roberto Butti <roberto.butti@gmail.com>
2024-08-06 22:28:08 +0200

    changelog

--- rector.php
+++ rector.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
-
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/examples',
-    __DIR__ . '/src',
-    __DIR__ . '/tests',
+        __DIR__.'/examples',
+        __DIR__.'/src',
+        __DIR__.'/tests',
     ])
     // uncomment to reach your current PHP version
     ->withPhpSets(
